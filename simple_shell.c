77472ffdcff213f1cb57a658502a45346404852f
OFHATWA <tofhatwa@gmail.com>
2023-11-15 11:35:29 -0800

    first

---
+++ simple_shell.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point for the simple shell.
+ *
+ * Return: 0 on success, 1 on failure.
+ */
+int main(void)
+{
+	char *command;
+
+	while (1)
+	{
+	printf("$ ");
+	command = read_command();
+
+	if (command != NULL)
+	{
+		if (execute_command(command) == -1)
+		{
+			perror("Error executing command");
+			free(command);
+			return (1);
+		}
+		free(command);
+	}
+	else
+	{
+		return (1);
+	}
+	}
+	return (0);
+}
+
+/**
+ * execute_command - Execute the user's command.
+ *
+ * @command: The command to execute.
+ * Return: 0 on success, -1 on failure.
+ */
+int execute_command(char *command)
+{
+	return (0);
+}
+
+/**
+ * read_command - Read a command from the user.
+ *
+ * Return: The user's command as a string, or NULL on failure.
+ */
+char *read_command(void)
+{
+	size_t bufsize = 1024;
+	char *buffer = malloc(bufsize);
+
+	if (buffer == NULL)
+	{
+		perror("Memory allocation failed");
+		return (NULL);
+	}
+
+	ssize_t characters_read = getline(&buffer, &bufsize, stdin);
+
+	if (characters_read == -1)
+	{
+		free(buffer);
+		return (NULL);
+	}
+
+	if (buffer[characters_read - 1] == '\n')
+	buffer[characters_read - 1] = '\0';
+
+	return (buffer);
+}
